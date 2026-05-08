8748edaeb98422fcc6b71c6d9bcf616108df4877
Murali Sepna <sepena4mobile@gmail.com>
2026-05-07 21:13:58 -0700

    Initial Commit

---
+++ LazyListDemo/LazyListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyListDemoApp.swift
+//  LazyListDemo
+//
+//  Created by Hema Sepena on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct LazyListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LazyListDemoTests/LazyListDemoTests.swift
@@ -0,0 +1,19 @@
+//
+//  LazyListDemoTests.swift
+//  LazyListDemoTests
+//
+//  Created by Hema Sepena on 5/7/26.
+//
+
+import Testing
+@testable import LazyListDemo
+
+struct LazyListDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
